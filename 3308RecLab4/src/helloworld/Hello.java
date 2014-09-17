7380798236d10e75c3b2bdb9ec85aa2b674a8526
Erik Eakins <erik.eakins@colorado.edu>
2014-09-17 13:45:27 -0600

    Update Hello.java

--- 3308RecLab4/src/helloworld/Hello.java
+++ 3308RecLab4/src/helloworld/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World!"); // Display the string
 		System.out.println("How are you?");
+		System.out.println("Laxit, I did it!!!!");
 	}
 }
